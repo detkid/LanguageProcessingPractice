98aa6deb804f87560fad06e71b26ba40c4a27a0c
ohira yutaro <yutaro@ohiranoMacBook-Air.local>
2017-07-28 15:44:04 +0900

    2,3

---
+++ LPP#1-10/#2.py
@@ -0,0 +1,4 @@
+st = 'パタトクカシーー'
+for i in range(len(list(st))):
+    if i % 2 == 1:
+        print(st[i])
---
+++ LPP#1-10/#3.py
@@ -0,0 +1,8 @@
+st1 = 'パトカー'
+st2 = 'タクシー'
+ans = ''
+
+for (a, b) in zip(st1, st2):
+    ans += a + b
+
+print(ans)
